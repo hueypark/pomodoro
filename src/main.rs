ee68771d3d9b18a32ce4d2680e1502d4539495d6
Jaewan Park <jaewan.huey.park@gmail.com>
2025-06-03 16:44:11 +0900

    Implement a simple pomodoro timer

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+use std::time::Duration;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(PomodoroTimer(Timer::new(
+            Duration::from_secs(5),
+            TimerMode::Once,
+        )))
+        .add_systems(Startup, setup)
+        .add_systems(Update, pomodoro_timer_system)
+        .run();
+}
+
+#[derive(Resource, Deref, DerefMut)]
+struct PomodoroTimer(Timer);
+
+fn setup(mut cmds: Commands) {
+    cmds.spawn((Camera2d, Transform::default(), GlobalTransform::default()));
+    cmds.spawn((
+        PomodoroText,
+        Text2d::new(""),
+        TextFont {
+            font_size: 80.0,
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::default(),
+    ));
+}
+
+#[derive(Component)]
+struct PomodoroText;
+
+fn pomodoro_timer_system(
+    time: Res<Time>,
+    mut timer: ResMut<PomodoroTimer>,
+    mut query: Query<(&mut Text2d, &mut TextFont), With<PomodoroText>>,
+    mut cmds: Commands,
+    asset_server: Res<AssetServer>,
+    mut end: Local<bool>,
+) {
+    if *end {
+        return;
+    }
+
+    if !timer.tick(time.delta()).just_finished() {
+        if timer.finished() {
+            return;
+        }
+        let secs = timer.remaining().as_secs();
+        let min = secs / 60;
+        let sec = secs % 60;
+        if let Ok((mut text, _)) = query.single_mut() {
+            text.0 = format!("{:02}:{:02}", min, sec);
+        }
+
+        return;
+    }
+
+    *end = true;
+
+    if let Ok((mut text, _)) = query.single_mut() {
+        text.0 = "Time's up!".to_string();
+    }
+
+    let a = asset_server.load::<AudioSource>("alarm.wav");
+    cmds.spawn(AudioPlayer(a));
+}
